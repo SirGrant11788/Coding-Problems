b4f21137b389920b81bcc597f82b8f8e85adb1bd
SirGrant11788 <grant.verheul@gmail.com>
2020-06-11 13:32:05 +0200

    web scrapper
    added menu and clean up

--- Coding Problems/WebScraping.cs
+++ Coding Problems/WebScraping.cs
@@ -16,12 +16,8 @@ namespace Coding_Problems
         
         public static void Scrape()
         {
-            //write files
-            
-            string file = @"F:\Google Drive\Visual Studio Projects\Coding Problems\Coding Problems\bin\Debug\netcoreapp3.0\jobs.txt";
-            File.Create(file).Close();
-            StreamWriter sw = new StreamWriter(file, true);
 
+            //demo start
             //npm install selenium-webdriver
             //npm install chromedriver
 
@@ -32,85 +28,115 @@ namespace Coding_Problems
             //chromeDriver.Keyboard.SendKeys("Test");
             //chromeDriver.Keyboard.SendKeys(Keys.Enter);
             //*[@id=\"search\"]input[1]
-            //
+            //demo end
+
+            string location = "Johannesburg, Gauteng";
+            string jobTitleOne = "junior developer";
+            string jobTitleTwo = "junior software";
+            string extraKeyword = "junior";
+            Console.WriteLine($"DEFAULTS\nLoacation: {location}\nJob title one: {jobTitleOne}\nJob title two: {jobTitleTwo}\nExtra keyword: {extraKeyword}\nChange defaults y/n");
+            string changeDefaults = Console.ReadLine();
+            if (changeDefaults.ToLower().Equals("y"))
+            {
+                Console.WriteLine("Enter Location: ");
+                location = Console.ReadLine();
+                Console.WriteLine("Enter Job Title One: ");
+                jobTitleOne = Console.ReadLine();
+                Console.WriteLine("Enter Job Title Two: ");
+                jobTitleTwo = Console.ReadLine();
+                Console.WriteLine("Enter Extra Keyword: ");
+                extraKeyword = Console.ReadLine();
+            }
+
+
+
+            string file = @"F:\Google Drive\Visual Studio Projects\Coding Problems\Coding Problems\bin\Debug\netcoreapp3.0\jobs.txt";
+            File.Create(file).Close();
+
+            StreamWriter sw = new StreamWriter(file, true);
 
             ChromeOptions options = new ChromeOptions();
             options.AddArguments("--disable-popup-blocking");
             options.AddArguments("test-type");
 
 
-            ////start Indeed 1
-            //IWebDriver driver = new ChromeDriver(options);
-            //driver.Url = "https://za.indeed.com/";
-            //IWebElement elementWhat = driver.FindElement(By.Id("text-input-what"));
-            //elementWhat.SendKeys("junior software");
-            //IWebElement elementWhere = driver.FindElement(By.Id("text-input-where"));
-            //elementWhere.SendKeys("Johannesburg, Gauteng");
-            //elementWhere.SendKeys(Keys.Enter);
-            //Indeed(driver,sw);
-            //driver.Close();
+            //start Indeed 1
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Url = "https://za.indeed.com/";
+            IWebElement elementWhat = driver.FindElement(By.Id("text-input-what"));
+            elementWhat.SendKeys(jobTitleTwo);
+            IWebElement elementWhere = driver.FindElement(By.Id("text-input-where"));
+            elementWhere.SendKeys(location);
+            elementWhere.SendKeys(Keys.Enter);
+            Indeed(driver, sw, extraKeyword);
+            driver.Close();
 
-            ////start Indeed 2
-            //IWebDriver driverTwo = new ChromeDriver(options);
-            //driverTwo.Url = "https://za.indeed.com/";
+            //start Indeed 2
+            IWebDriver driverTwo = new ChromeDriver(options);
+            driverTwo.Url = "https://za.indeed.com/";
+            IWebElement elementWhatTwo = driverTwo.FindElement(By.Id("text-input-what"));
+            elementWhatTwo.SendKeys(jobTitleOne);
+            IWebElement elementWhereTwo = driverTwo.FindElement(By.Id("text-input-where"));
+            elementWhereTwo.SendKeys(location);
+            elementWhereTwo.SendKeys(Keys.Enter);
+            Indeed(driverTwo, sw, extraKeyword);
+            driverTwo.Close();
 
-            //IWebElement elementWhatTwo = driverTwo.FindElement(By.Id("text-input-what"));
-            //elementWhatTwo.SendKeys("junior developer");
-            //IWebElement elementWhereTwo = driverTwo.FindElement(By.Id("text-input-where"));
-            //elementWhereTwo.SendKeys("Johannesburg, Gauteng");
-            //elementWhereTwo.SendKeys(Keys.Enter);
-            //Indeed(driverTwo,sw);
-            //driverTwo.Close();
+            //start Careerjunction
+            IWebDriver driverCareerjunction = new ChromeDriver(options);
+            //js dropdown issue
+            driverCareerjunction.Url = "https://www.careerjunction.co.za/jobs/results?jobtitle=&companies=&job_ref=&keywords=junior+developer&location%5B%5D=2747";
+            Careerjunction(driverCareerjunction, sw);
+            driverCareerjunction.Close();
 
-            ////start Careerjunction
-            //IWebDriver driverCareerjunction = new ChromeDriver(options);
-            ////js dropdown issue
-            //driverCareerjunction.Url = "https://www.careerjunction.co.za/jobs/results?jobtitle=&companies=&job_ref=&keywords=junior+developer&location%5B%5D=2747";
-            //Careerjunction(driverCareerjunction,sw);
-            //driverCareerjunction.Close();
+            //start Pnet
+            IWebDriver driverFour = new ChromeDriver(options);
+            driverFour.Url = "https://www.pnet.co.za/";
+            IWebElement elementWhatPnet = driverFour.FindElement(By.XPath("//*[@id=\"collapseSearch\"]/div/div/div/div/form/div[1]/div[1]/div/span[2]/input[2]"));
+            elementWhatPnet.SendKeys(jobTitleOne);
+            IWebElement elementWherePnet = driverFour.FindElement(By.XPath("//*[@id=\"collapseSearch\"]/div/div/div/div/form/div[1]/div[2]/div/div/span[2]/input[2]"));
+            elementWherePnet.SendKeys(location);
+            elementWhatPnet.SendKeys(Keys.Enter);
+            int pageCount = 0;
+            Pnet(driverFour, pageCount, sw, extraKeyword);
+            driverFour.Close();
 
-            ////start Pnet
-            //IWebDriver driverFour = new ChromeDriver(options);
-            //driverFour.Url = "https://www.pnet.co.za/";
-            //IWebElement elementWhatPnet = driverFour.FindElement(By.XPath("//*[@id=\"collapseSearch\"]/div/div/div/div/form/div[1]/div[1]/div/span[2]/input[2]"));
-            //elementWhatPnet.SendKeys("junior developer");
-            //IWebElement elementWherePnet = driverFour.FindElement(By.XPath("//*[@id=\"collapseSearch\"]/div/div/div/div/form/div[1]/div[2]/div/div/span[2]/input[2]"));
-            //elementWherePnet.SendKeys("Johannesburg, Gauteng");
-            //elementWhatPnet.SendKeys(Keys.Enter);
-            //int pageCount = 0;
-            //Pnet(driverFour,pageCount,sw);
-            //driverFour.Close();
-
-            ////start CareerJet
-            //IWebDriver driverFive = new ChromeDriver(options);
-            //driverFive.Url = "https://www.careerjet.co.za/";
-            //IWebElement elementWhatCareerjet = driverFive.FindElement(By.Id("s"));
-            //elementWhatCareerjet.SendKeys("junior software");
-            //IWebElement elementWhereCareerjet = driverFive.FindElement(By.Id("l"));
-            //elementWhereCareerjet.SendKeys("Johannesburg, Gauteng");
-            //elementWhereCareerjet.SendKeys(Keys.Enter);
-            //Careerjet(driverFive, sw);
-            //driverFive.Close();
+            //start CareerJet
+            IWebDriver driverFive = new ChromeDriver(options);
+            driverFive.Url = "https://www.careerjet.co.za/";
+            IWebElement elementWhatCareerjet = driverFive.FindElement(By.Id("s"));
+            elementWhatCareerjet.SendKeys(jobTitleOne);
+            IWebElement elementWhereCareerjet = driverFive.FindElement(By.Id("l"));
+            elementWhereCareerjet.SendKeys(location);
+            elementWhereCareerjet.SendKeys(Keys.Enter);
+            Careerjet(driverFive, sw, extraKeyword);
+            driverFive.Close();
 
             //start Careers24
             IWebDriver driverSix = new ChromeDriver(options);
-            //driverSix.Url = "https://www.careers24.com/";
-            driverSix.Url = "https://www.careers24.com/jobs/lc-johannesburg/kw-junior-developer/m-true/";
-            
-            //driverSix.FindElement(By.XPath("//*[contains(@name, 'Johannesburg')]")).Click();
 
+            //driverSix.Url = "https://www.careers24.com/";
             //IWebElement elementWhatCareers24 = driverSix.FindElement(By.Id("inSearch"));
             //elementWhatCareers24.SendKeys("junior developer");
-            //elementWhatCareers24.SendKeys(Keys.Enter);
+            //todo menu select
+            
+
+            driverSix.Url = "https://www.careers24.com/jobs/lc-johannesburg/kw-junior-developer/m-true/";
+
+
             Careers24(driverSix, sw);
-            //driverSix.Close();
+            driverSix.Close();
 
             //end bit
             sw.Close();
-            //Process.Start("notepad.exe", file);
+            Process.Start("notepad.exe", file);//todo notepad++
 
             //this.Close();
 
+            //driverSix.FindElement(By.XPath("//*[contains(@name, 'Johannesburg')]")).Click();
+            //IWebElement elementWhatCareers24 = driverSix.FindElement(By.Id("inSearch"));
+            //elementWhatCareers24.SendKeys("junior developer");
+            //elementWhatCareers24.SendKeys(Keys.Enter);
             //////IWebElement elementWhere = driverFive.FindElement(By.Name("industries"));
             ////driverFive.FindElement(By.XPath("/html/body/div[5]/div[1]/div[1]/form/div[2]/select/option[10]")).Click();
             ////driverFive.FindElement(By.XPath("//*[@id=\"city\"]/option[4]")).Click();
@@ -128,7 +154,7 @@ namespace Coding_Problems
             {
                 Console.WriteLine("\n\n"+item.Text);
                 Console.WriteLine(item.GetAttribute("href"));
-
+                sw.WriteLine("\n\n" + item.Text+"\n"+ item.GetAttribute("href"));
             }
             try
             {
@@ -142,7 +168,7 @@ namespace Coding_Problems
             }
         }
 
-        public static void Careerjet(IWebDriver driver, StreamWriter sw)
+        public static void Careerjet(IWebDriver driver, StreamWriter sw, string extraKeyword)
         {
 
             for (int i = 0; i < 6; i++)
@@ -157,7 +183,7 @@ namespace Coding_Problems
 
             foreach (var item in titles)
             {
-                if (item.Text.ToLower().Contains("junior")) 
+                if (item.Text.ToLower().Contains(extraKeyword)) 
                 {
                     Console.WriteLine("\n\n" + item.Text);
                     Console.WriteLine("https://www.careerjet.co.za/" + item.GetAttribute("data-url"));
@@ -170,7 +196,7 @@ namespace Coding_Problems
             
         }
 
-        public static void Pnet(IWebDriver driver, int pageCount, StreamWriter sw)
+        public static void Pnet(IWebDriver driver, int pageCount, StreamWriter sw, string extraKeyword)
         {
             pageCount++;
             try
@@ -184,16 +210,12 @@ namespace Coding_Problems
                 Console.WriteLine("NO POP DISMISS!\n"+ex);
             }
 
-            //var titles = driver.FindElements(By.ClassName("col-lg-9"));
             var titles = driver.FindElements(By.XPath("//*[contains(@id, 'job-item')]"));
             var link = driver.FindElements(By.XPath("//*[contains(@href, '/jobs')]"));
-            //var summary = driver.FindElements(By.ClassName("summary"));
-            //var daysAgo = driver.FindElements(By.ClassName("jobsearch-SerpJobCard-footer"));
-            //var link = driver.FindElements(By.TagName("a"));
             
             for (int i = 0; i < titles.Count; i++)
             {
-                if (titles[i].Text.ToLower().Contains("junior"))
+                if (titles[i].Text.ToLower().Contains(extraKeyword))
                 {
                     Console.WriteLine("\n\n" + titles[i].Text);
                     sw.WriteLine("\n\n" + titles[i].Text);
@@ -210,14 +232,12 @@ namespace Coding_Problems
 
             try
             {
-                //driver.FindElement(By.Id("Next")).Click();//title
-                //*[@id="app-dynamicResultlist"]/div/div[1]/div/div[2]/div[3]/div[3]/a[2]
 
                 if(pageCount <= 20)
                 {
                     driver.FindElement(By.XPath("//*[contains(@title, 'Next')]")).Click();
 
-                    Pnet(driver, pageCount, sw);
+                    Pnet(driver, pageCount, sw, extraKeyword);
                 }
 
                 
@@ -241,7 +261,7 @@ namespace Coding_Problems
             }
         }
 
-        public static void Indeed(IWebDriver driver, StreamWriter sw)
+        public static void Indeed(IWebDriver driver, StreamWriter sw, string extraKeyword)
         {
             var titles = driver.FindElements(By.ClassName("title"));
             var summary = driver.FindElements(By.ClassName("summary"));
@@ -250,7 +270,7 @@ namespace Coding_Problems
 
             for (int i = 0; i < titles.Count; i++)
             {
-                if (!daysAgo[i].Text.Contains("30"))//&& titles[i].Text.ToLower().Contains("junior")
+                if (!daysAgo[i].Text.Contains("30"))//&& titles[i].Text.ToLower().Contains(extraKeyword)
                 {
                     Console.WriteLine("\n\n" + titles[i].Text + "\n" + summary[i].Text + "\n" + daysAgo[i].Text);
                     sw.WriteLine("\n\n" + titles[i].Text + "\n" + summary[i].Text + "\n" + daysAgo[i].Text);
@@ -268,19 +288,9 @@ namespace Coding_Problems
             try
             {
                 Console.WriteLine("POP!");
-                //IWebElement element = driver.FindElement(By.XPath("//*[@id=\"popover - link - x\"]"));
-                //element.Click();
-                //driver.Refresh();
                 IWebElement popup = driver.FindElement(By.Id("popover-email"));
                 popup.SendKeys(Keys.Escape);
-                //driver.SwitchTo().Alert().SendKeys(Keys.Escape);
                 Console.WriteLine("POP!!");
-                //if (element.Displayed && element.Enabled)
-                //{
-                //    Console.WriteLine("POP");
-                //    element.Click();
-                //    Console.WriteLine("POP BYE");
-                //}
 
             }
             catch (Exception ex)
@@ -288,13 +298,12 @@ namespace Coding_Problems
                 Console.WriteLine("NO POP!");
             }
                 Console.WriteLine("\nEND!");
-                //Console.WriteLine(ex + "\n");
                 try
                 {
 
                     driver.FindElement(By.XPath("//*[@id=\"resultsCol\"]/nav/div/ul/li[7]/a")).Click();
                     Console.WriteLine("CLICK!");
-                    Indeed(driver, sw);
+                    Indeed(driver, sw, extraKeyword);
 
 
                 } catch (Exception exc) {
@@ -302,19 +311,14 @@ namespace Coding_Problems
                     {
                         driver.FindElement(By.XPath("//*[@id=\"resultsCol\"]/nav/div/ul/li[6]/a")).Click();
                         Console.WriteLine("\nCLICK!!");
-                        Indeed(driver, sw);
+                        Indeed(driver, sw, extraKeyword);
                     }
                     catch (Exception e)
                     {
                         Console.WriteLine("\nEND!!");
-                        //Console.WriteLine(e);
                     }
 
                 }
-            
-            //}
-            
-            
         }
     }
 }
